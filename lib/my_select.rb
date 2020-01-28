a26885008f25a3f408391a4e04d9a966099854f3
DrVonDevious <drvondevious@gmail.com>
2020-01-27 20:42:20 -0600

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
+
+test = [1,2,3,4,5,6,7,8]
+
 def my_select(collection)
- # your code here!
+  result = []
+  i = 0
+  while i < collection.length do
+    result << collection[i] if yield(collection[i])
+    i += 1
+  end  
+  result
 end
+
+puts my_select(test) { |item| item.odd? }
